6beaeb54822bfa376e05ad4035dc3acc18d065af
Giandomenico Olini <giando.olini@gogodigital.it>
2026-01-30 22:06:03 +0100

    Update SidebarMenu.php

--- widgets/SidebarMenu.php
+++ widgets/SidebarMenu.php
@@ -1,14 +1,14 @@
 <?php
 
 /**
-* @copyright Copyright &copy; Gogodigital Srls
-* @company Gogodigital Srls - Wide ICT Solutions 
-* @website http://www.gogodigital.it
-* @github https://github.com/cinghie/yii2-adminlte3
-* @license GNU GENERAL PUBLIC LICENSE VERSION 3
-* @package yii2-AdminLTE
-* @version 0.1.0
-*/
+ * @copyright Copyright &copy; Gogodigital Srls
+ * @company Gogodigital Srls - Wide ICT Solutions
+ * @website http://www.gogodigital.it
+ * @github https://github.com/cinghie/yii2-adminlte3
+ * @license GNU GENERAL PUBLIC LICENSE VERSION 3
+ * @package yii2-AdminLTE
+ * @version 0.1.0
+ */
 
 namespace cinghie\adminlte3\widgets;
 
@@ -19,260 +19,301 @@ use yii\helpers\Html;
 use yii\widgets\Menu;
 
 /**
- * Class SidebarMenu
+ * SidebarMenu widget for AdminLTE 3 with Bootstrap 4.
+ *
+ * Renders a sidebar navigation menu compatible with AdminLTE 3 treeview plugin.
+ * Uses Font Awesome 5 icon classes (fas, far, fab).
+ *
+ * @see https://adminlte.io/docs/3.1/components/main-sidebar.html
  */
 class SidebarMenu extends Menu
 {
-	/**
-	 * @var string
-	 **/
-	public $linkTemplate = '<a class="nav-link" href="{url}">{icon} {label}</a>';
+    /**
+     * @var string Template for menu links (AdminLTE 3: icon + label in <p>)
+     */
+    public $linkTemplate = '<a class="nav-link" href="{url}">{icon}<p>{label}{caret}{badge}</p></a>';
 
-	/**
-	 * @var string
-	 **/
-	public $labelTemplate = '<span>{label}</span>';
+    /**
+     * @var string Template for label-only items (e.g. headers). Use {label} for AdminLTE 3.
+     */
+    public $labelTemplate = '{label}';
 
-	/**
-	 * @var string
-	 **/
-	public $submenuTemplate = "\n<ul class='nav nav-treeview' {show}>\n{items}\n</ul>\n";
+    /**
+     * @var string Template for parent items with submenu (icon + label + angle icon)
+     */
+    public $parentLinkTemplate = '<a class="nav-link" href="{url}">{icon}<p>{label}<i class="right fas fa-angle-left"></i>{badge}</p></a>';
 
-	/**
-	 * @var boolean
-	 **/
-	public $activateParents = true;
+    /**
+     * @var string Template for submenu container (AdminLTE 3 nav-treeview)
+     */
+    public $submenuTemplate = "\n<ul class=\"nav nav-treeview\" {show}>\n{items}\n</ul>\n";
 
-	/**
-	 * @var string
-	 **/
-	public $defaultIconHtml = '';
+    /**
+     * @var bool Whether to activate parent menu items when a child is active
+     */
+    public $activateParents = true;
 
-	/**
-	 * @var array
-	 **/
-	public $options = ['class' => 'nav nav-pills nav-sidebar flex-column', 'data-accordion' => 'false','data-widget' => 'treeview', 'role' => 'menu'];
+    /**
+     * @var string Default HTML for icon when item has no icon
+     */
+    public $defaultIconHtml = '';
 
-	/**
-	 * @var string
-	 */
-	public static $iconClassPrefix = '';
+    /**
+     * @var array Options for the root ul (AdminLTE 3 treeview)
+     */
+    public $options = [
+        'class' => 'nav nav-pills nav-sidebar flex-column',
+        'data-widget' => 'treeview',
+        'role' => 'menu',
+        'data-accordion' => 'false',
+    ];
 
-	/**
-	 * @var boolean
-	 **/
-	private $noDefaultAction;
+    /**
+     * @var string Prefix for icon classes (e.g. 'nav-icon ' for AdminLTE 3)
+     */
+    public static $iconClassPrefix = 'nav-icon ';
 
-	/**
-	 * @var boolean
-	 **/
-	private $noDefaultRoute;
+    /**
+     * @var string Default icon class for submenu items (AdminLTE 3 uses far fa-circle)
+     */
+    public $submenuIconClass = 'far fa-circle nav-icon';
 
-	/**
-	 * Renders the menu.
-	 */
-	public function run()
-	{
-		if ($this->route === null && Yii::$app->controller !== null) {
-			$this->route = Yii::$app->controller->getRoute();
-		}
+    /**
+     * @var bool
+     */
+    private $noDefaultAction;
 
-		if ($this->params === null) {
-			$this->params = Yii::$app->request->getQueryParams();
-		}
+    /**
+     * @var bool
+     */
+    private $noDefaultRoute;
 
-		$posDefaultAction = strpos($this->route, Yii::$app->controller->defaultAction);
+    /**
+     * {@inheritdoc}
+     */
+    public function run()
+    {
+        if ($this->route === null && Yii::$app->controller !== null) {
+            $this->route = Yii::$app->controller->getRoute();
+        }
 
-		if ($posDefaultAction) {
-			$this->noDefaultAction = rtrim(substr($this->route, 0, $posDefaultAction), '/');
-		} else {
-			$this->noDefaultAction = false;
-		}
+        if ($this->params === null) {
+            $this->params = Yii::$app->request->getQueryParams();
+        }
 
-		$posDefaultRoute = strpos($this->route, Yii::$app->controller->module->defaultRoute);
+        $posDefaultAction = strpos($this->route, Yii::$app->controller->defaultAction);
+        if ($posDefaultAction) {
+            $this->noDefaultAction = rtrim(substr($this->route, 0, $posDefaultAction), '/');
+        } else {
+            $this->noDefaultAction = false;
+        }
 
-		if ($posDefaultRoute) {
-			$this->noDefaultRoute = rtrim(substr($this->route, 0, $posDefaultRoute), '/');
-		} else {
-			$this->noDefaultRoute = false;
-		}
+        if (Yii::$app->controller->module !== null) {
+            $posDefaultRoute = strpos($this->route, Yii::$app->controller->module->defaultRoute);
+            if ($posDefaultRoute) {
+                $this->noDefaultRoute = rtrim(substr($this->route, 0, $posDefaultRoute), '/');
+            } else {
+                $this->noDefaultRoute = false;
+            }
+        } else {
+            $this->noDefaultRoute = false;
+        }
 
-		$items = $this->normalizeItems($this->items, $hasActiveChild);
+        $items = $this->normalizeItems($this->items, $hasActiveChild);
 
-		if (!empty($items))
-		{
-			$options = $this->options;
-			$tag = ArrayHelper::remove($options, 'tag', 'ul');
-			echo Html::tag($tag, $this->renderItems($items), $options);
-		}
-	}
+        if (!empty($items)) {
+            $options = $this->options;
+            $tag = ArrayHelper::remove($options, 'tag', 'ul');
+            echo Html::tag($tag, $this->renderItems($items), $options);
+        }
+    }
 
-	/**
-	 * @inheritdoc
-	 */
-	protected function renderItem($item)
-	{
-		if (isset($item['items'])) {
-			$labelTemplate = '<a href="{url}">{icon} {label} <span class="pull-right-container"><i class="fa fa-angle-left pull-right"></i></span></a>';
-			$linkTemplate = '<a href="{url}">{icon} {label} <span class="pull-right-container"><i class="fa fa-angle-left pull-right"></i></span></a>';
-		} else {
-			$labelTemplate = $this->labelTemplate;
-			$linkTemplate = $this->linkTemplate;
-		}
+    /**
+     * {@inheritdoc}
+     */
+    protected function renderItem($item)
+    {
+        $isHeader = isset($item['options']['class']) && strpos($item['options']['class'], 'nav-header') !== false;
+        if ($isHeader) {
+            return strtr($this->labelTemplate, ['{label}' => $item['label']]);
+        }
 
-		$replacements = [
-			'{label}' => strtr($this->labelTemplate, ['{label}' => $item['label'],]),
-			'{icon}' => empty($item['icon']) ? $this->defaultIconHtml
-				: '<i class="' . self::$iconClassPrefix . $item['icon'] . '"></i> ',
-			'{url}' => isset($item['url']) ? Url::to($item['url']) : 'javascript:void(0);',
-		];
+        $hasItems = !empty($item['items']);
+        $caret = $hasItems ? '' : '';
+        $badge = ArrayHelper::getValue($item, 'badge', '');
+        if ($badge !== '') {
+            $badgeOptions = ArrayHelper::getValue($item, 'badgeOptions', ['class' => 'right badge badge-danger']);
+            $badge = ' ' . Html::tag('span', $badge, $badgeOptions);
+        }
 
-		$template = ArrayHelper::getValue($item, 'template', isset($item['url']) ? $linkTemplate : $labelTemplate);
+        if ($hasItems) {
+            $template = ArrayHelper::getValue($item, 'template', $this->parentLinkTemplate);
+        } else {
+            $template = ArrayHelper::getValue($item, 'template', $this->linkTemplate);
+        }
 
-		return strtr($template, $replacements);
-	}
+        $icon = empty($item['icon'])
+            ? $this->defaultIconHtml
+            : '<i class="' . static::$iconClassPrefix . $item['icon'] . '"></i> ';
+        $url = isset($item['url']) ? Url::to($item['url']) : '#';
+        $label = strtr($this->labelTemplate, ['{label}' => $item['label']]);
 
-	/**
-	 * Recursively renders the menu items (without the container tag)
+        return strtr($template, [
+            '{url}' => $url,
+            '{label}' => $label,
+            '{icon}' => $icon,
+            '{caret}' => $caret,
+            '{badge}' => $badge,
+        ]);
+    }
+
+    /**
+     * Recursively renders the menu items.
      *
-	 * @param array $items the menu items to be rendered recursively
+     * @param array $items the menu items to be rendered recursively
+     * @return string the rendering result
+     */
+    protected function renderItems($items)
+    {
+        $n = count($items);
+        $lines = [];
+
+        foreach ($items as $i => $item) {
+            $options = array_merge($this->itemOptions, ArrayHelper::getValue($item, 'options', []));
+            $tag = ArrayHelper::remove($options, 'tag', 'li');
+
+            $isHeader = isset($options['class']) && strpos($options['class'], 'nav-header') !== false;
+            if ($isHeader) {
+                $lines[] = Html::tag($tag, $this->renderItem($item), $options);
+                continue;
+            }
+
+            $class = [];
+            if ($item['active']) {
+                $class[] = $this->activeCssClass;
+            }
+            if (!empty($item['items']) && $item['active']) {
+                $class[] = 'menu-open';
+            }
+            if ($i === 0 && $this->firstItemCssClass !== null) {
+                $class[] = $this->firstItemCssClass;
+            }
+            if ($i === $n - 1 && $this->lastItemCssClass !== null) {
+                $class[] = $this->lastItemCssClass;
+            }
+            $class[] = 'nav-item';
+
+            if (!empty($class)) {
+                if (empty($options['class'])) {
+                    $options['class'] = implode(' ', $class);
+                } else {
+                    $options['class'] .= ' ' . implode(' ', $class);
+                }
+            }
+
+            $menu = $this->renderItem($item);
+
+            if (!empty($item['items'])) {
+                $menu .= strtr($this->submenuTemplate, [
+                    '{show}' => $item['active'] ? 'style="display: block;"' : '',
+                    '{items}' => $this->renderItems($item['items']),
+                ]);
+            }
+
+            $lines[] = Html::tag($tag, $menu, $options);
+        }
+
+        return implode("\n", $lines);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function normalizeItems($items, &$active)
+    {
+        foreach ($items as $i => $item) {
+            if (isset($item['visible']) && !$item['visible']) {
+                unset($items[$i]);
+                continue;
+            }
+
+            if (!isset($item['label'])) {
+                $item['label'] = '';
+            }
+
+            $encodeLabel = isset($item['encode']) ? $item['encode'] : $this->encodeLabels;
+            $items[$i]['label'] = $encodeLabel ? Html::encode($item['label']) : $item['label'];
+            $items[$i]['icon'] = isset($item['icon']) ? $item['icon'] : '';
+            $items[$i]['options'] = ArrayHelper::getValue($item, 'options', []);
+            $items[$i]['badge'] = ArrayHelper::getValue($item, 'badge', '');
+            $items[$i]['badgeOptions'] = ArrayHelper::getValue($item, 'badgeOptions', ['class' => 'right badge badge-danger']);
+            $hasActiveChild = false;
+
+            if (isset($item['items'])) {
+                $items[$i]['items'] = $this->normalizeItems($item['items'], $hasActiveChild);
+                $isHeader = isset($items[$i]['options']['class']) && strpos($items[$i]['options']['class'], 'nav-header') !== false;
+                if (empty($items[$i]['items']) && $this->hideEmptyItems && !$isHeader) {
+                    unset($items[$i]['items']);
+                    if (!isset($item['url'])) {
+                        unset($items[$i]);
+                        continue;
+                    }
+                }
+            }
+
+            if (!isset($item['active'])) {
+                if (($this->activateParents && $hasActiveChild) || ($this->activateItems && $this->isItemActive($item))) {
+                    $active = $items[$i]['active'] = true;
+                } else {
+                    $items[$i]['active'] = false;
+                }
+            } elseif ($item['active']) {
+                $active = true;
+            }
+        }
+
+        return array_values($items);
+    }
+
+    /**
+     * Checks whether a menu item is active.
      *
-	 * @return string the rendering result
-	 */
-	protected function renderItems($items)
-	{
-		$n = count($items);
-		$lines = [];
+     * @param array $item the menu item to be checked
+     * @return bool whether the menu item is active
+     */
+    protected function isItemActive($item)
+    {
+        if (isset($item['url']) && is_array($item['url']) && isset($item['url'][0])) {
+            $route = $item['url'][0];
 
-		foreach ($items as $i => $item)
-		{
-			$options = array_merge($this->itemOptions, ArrayHelper::getValue($item, 'options', []));
-			$tag = ArrayHelper::remove($options, 'tag', 'li');
-			$class = [];
+            if ($route[0] !== '/' && Yii::$app->controller !== null) {
+                $route = ltrim(
+                    Yii::$app->controller->module !== null
+                        ? Yii::$app->controller->module->getUniqueId() . '/' . $route
+                        : $route,
+                    '/'
+                );
+            }
 
-			if ($item['active']) {
-				$class[] = $this->activeCssClass;
-			}
+            $route = ltrim($route, '/');
 
-			if ($i === 0 && $this->firstItemCssClass !== null) {
-				$class[] = $this->firstItemCssClass;
-			}
+            if ($route !== $this->route && $route !== $this->noDefaultRoute && $route !== $this->noDefaultAction) {
+                return false;
+            }
 
-			if ($i === $n - 1 && $this->lastItemCssClass !== null) {
-				$class[] = $this->lastItemCssClass;
-			}
+            unset($item['url']['#']);
 
-			if (!empty($class))
-			{
-				if (empty($options['class'])) {
-					$options['class'] = implode(' ', $class);
-				} else {
-					$options['class'] .= ' ' . implode(' ', $class);
-				}
-			}
+            if (count($item['url']) > 1) {
+                foreach (array_splice($item['url'], 1) as $name => $value) {
+                    if ($value !== null && (!isset($this->params[$name]) || $this->params[$name] !== $value)) {
+                        return false;
+                    }
+                }
+            }
 
-			$menu = $this->renderItem($item);
+            return true;
+        }
 
-			if (!empty($item['items']))
-			{
-				$menu .= strtr($this->submenuTemplate, [
-					'{show}' => $item['active'] ? "style='display: block'" : '',
-					'{items}' => $this->renderItems($item['items']),
-				]);
-				if (isset($options['class'])) {
-					$options['class'] .= ' nav-item';
-				} else {
-					$options['class'] = 'nav-item';
-				}
-			}
-
-			$lines[] = Html::tag($tag, $menu, $options);
-		}
-
-		return implode("\n", $lines);
-	}
-
-	/**
-	 * @inheritdoc
-	 */
-	protected function normalizeItems($items, &$active)
-	{
-		foreach ($items as $i => $item)
-		{
-			if (isset($item['visible']) && !$item['visible']) {
-				unset($items[$i]);
-				continue;
-			}
-
-			if (!isset($item['label'])) {
-				$item['label'] = '';
-			}
-
-			$encodeLabel = isset($item['encode']) ? $item['encode'] : $this->encodeLabels;
-			$items[$i]['label'] = $encodeLabel ? Html::encode($item['label']) : $item['label'];
-			$items[$i]['icon'] = isset($item['icon']) ? $item['icon'] : '';
-			$hasActiveChild = false;
-
-			if (isset($item['items'])) {
-				$items[$i]['items'] = $this->normalizeItems($item['items'], $hasActiveChild);
-				if (empty($items[$i]['items']) && $this->hideEmptyItems) {
-					unset($items[$i]['items']);
-					if (!isset($item['url'])) {
-						unset($items[$i]);
-						continue;
-					}
-				}
-			}
-
-			if (!isset($item['active']))
-			{
-				if ($this->activateParents && $hasActiveChild || $this->activateItems && $this->isItemActive($item)) {
-					$active = $items[$i]['active'] = true;
-				} else {
-					$items[$i]['active'] = false;
-				}
-			} elseif ($item['active']) {
-				$active = true;
-			}
-		}
-
-		return array_values($items);
-	}
-
-	/**
-	 * @param array $item the menu item to be checked
-	 *
-	 * @return boolean whether the menu item is active
-	 */
-	protected function isItemActive($item)
-	{
-		if (isset($item['url']) && is_array($item['url']) && isset($item['url'][0]))
-		{
-			$route = $item['url'][0];
-
-			if ($route[0] !== '/' && Yii::$app->controller) {
-				$route = ltrim(Yii::$app->controller->module->getUniqueId() . '/' . $route, '/');
-			}
-
-			$route = ltrim($route, '/');
-
-			if ($route !== $this->route && $route !== $this->noDefaultRoute && $route !== $this->noDefaultAction) {
-				return false;
-			}
-
-			unset($item['url']['#']);
-
-			if (count($item['url']) > 1) {
-				foreach (array_splice($item['url'], 1) as $name => $value) {
-					if ($value !== null && (!isset($this->params[$name]) || $this->params[$name] !== $value)) {
-						return false;
-					}
-				}
-			}
-
-			return true;
-		}
-
-		return false;
-	}
+        return false;
+    }
 }
