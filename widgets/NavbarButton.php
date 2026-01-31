e2100b9a0a4922d784205e9a4085bab6d80b8cc6
Giandomenico Olini <giando.olini@gogodigital.it>
2026-01-31 10:17:29 +0100

    Update NavbarButton.php

--- widgets/NavbarButton.php
+++ widgets/NavbarButton.php
@@ -14,63 +14,78 @@ namespace cinghie\adminlte3\widgets;
 
 use yii\bootstrap4\Widget;
 use yii\helpers\Html;
+use yii\helpers\Url;
 
 /**
- * Class NavbarButton
+ * NavbarButton widget for AdminLTE 3 with Bootstrap 4.
+ *
+ * Renders a single navbar link (nav-item > nav-link).
+ * Use inside: <ul class="navbar-nav"> ... </ul>
+ *
+ * @see https://adminlte.io/docs/3.1/components/navbar.html
  */
 class NavbarButton extends Widget
 {
     /**
-     * @var string
+     * @var string Link content (HTML allowed, e.g. icon + text). Default: icon only.
      */
     public $title;
 
     /**
-     * @var array
+     * @var string|array Link URL (array will be passed to Url::to())
      */
-    public $option;
+    public $url = '#';
 
     /**
-     * @var string
+     * @var string|null Target attribute (e.g. '_blank'). Null = not set.
      */
     public $target;
 
     /**
-     * @var string
+     * @var array HTML options for the anchor (class => 'nav-link' is merged by default)
      */
-    public $url;
+    public $option = [];
 
-	/**
-	 * @inheritdoc
-	 */
+    /**
+     * @var bool Whether to wrap in <li class="nav-item">
+     */
+    public $renderAsLi = true;
+
+    /**
+     * @inheritdoc
+     */
     public function init()
     {
         if ($this->title === null) {
-            $this->title = '<i class="fa fa-external-link"></i>';
+            $this->title = '<i class="fas fa-external-link-alt"></i>';
         }
-
         if ($this->option === null) {
-            $this->option = ['class' => 'nav-link'];
+            $this->option = [];
         }
-
-        if ($this->target === null) {
-            $this->target = '';
-        } else {
-            $this->target = ' target="'.$this->target.'"';
-        }
-
-        if ($this->url === null) {
-            $this->url = 'https://www.google.com';
-        }
-
         parent::init();
     }
 
-	/**
-	 * @return string
-	 */
-	public function run()
+    /**
+     * @return string
+     */
+    public function run()
     {
-        return '<li class="nav-item">'.Html::a($this->title, Html::encode($this->url), $this->option).'</li>';
+        $url = is_array($this->url) ? Url::to($this->url) : $this->url;
+
+        $options = array_merge(['class' => 'nav-link'], $this->option);
+        if ($this->target !== null && $this->target !== '') {
+            $options['target'] = $this->target;
+            if ($this->target === '_blank') {
+                $options['rel'] = 'noopener noreferrer';
+            }
+        }
+
+        $link = Html::a($this->title, $url, $options);
+
+        if ($this->renderAsLi) {
+            return Html::tag('li', $link, ['class' => 'nav-item']);
+        }
+
+        return $link;
     }
 }
