38aefb22af1abd753c97287706952503cb4fdd43
Giandomenico Olini <giando.olini@gogodigital.it>
2026-01-31 10:17:36 +0100

    Update NavbarUser.php

--- widgets/NavbarUser.php
+++ widgets/NavbarUser.php
@@ -14,148 +14,140 @@ namespace cinghie\adminlte3\widgets;
 
 use yii\bootstrap4\Widget;
 use yii\helpers\Html;
+use yii\helpers\Url;
 
 /**
- * Class NavbarUser
+ * NavbarUser widget for AdminLTE 3 with Bootstrap 4.
+ *
+ * Renders the user dropdown in the navbar: image + username, dropdown with header (image + name + date),
+ * optional body links (e.g. Followers, Sales, Friends), and footer (Profile, Sign out).
+ * Uses AdminLTE 3 user-menu structure: .user-menu > .dropdown-menu > li.user-header, li.user-body, li.user-footer.
+ *
+ * @see https://adminlte.io/docs/3.1/components/navbar.html
  */
 class NavbarUser extends Widget
 {
     /**
-     * @var string
+     * @var string Username (navbar toggle and header)
      */
-    public $username;
+    public $username = '';
 
     /**
-     * @var string
+     * @var string|null URL of user image
      */
     public $userimg;
 
     /**
-     * @var string
+     * @var string|null Subtitle in header (e.g. "Member since Nov. 2012")
      */
     public $usercreated;
 
     /**
-     * @var boolean
+     * @var bool Whether to show the body block (three links)
      */
-    public $userbody;
+    public $userbody = false;
 
     /**
-     * @var string
+     * @var string|null Body link 1 label
      */
     public $userbodyname1;
 
     /**
-     * @var string
+     * @var string|array|null Body link 1 URL
      */
     public $userbodylink1;
 
     /**
-     * @var string
+     * @var string|null Body link 2 label
      */
     public $userbodyname2;
 
     /**
-     * @var string
+     * @var string|array|null Body link 2 URL
      */
     public $userbodylink2;
 
     /**
-     * @var string
+     * @var string|null Body link 3 label
      */
     public $userbodyname3;
 
     /**
-     * @var string
+     * @var string|array|null Body link 3 URL
      */
     public $userbodylink3;
 
     /**
-     * @var boolean
+     * @var bool Whether to show the footer (Profile, Sign out)
      */
-    public $userfooter;
+    public $userfooter = true;
 
     /**
-     * @var string
+     * @var string|null Footer left button label (e.g. "Profile")
      */
     public $userfootername1;
 
     /**
-     * @var string
+     * @var string|array|null Footer left button URL
      */
     public $userfooterlink1;
 
     /**
-     * @var string
+     * @var string|null Footer right button label (e.g. "Sign Out")
      */
     public $userfootername2;
 
     /**
-     * @var string
+     * @var string|array|null Footer right button URL
      */
     public $userfooterlink2;
 
-	/**
-	 * @inheritdoc
-	 */
+    /**
+     * @var array HTML options for the dropdown toggle link
+     */
+    public $linkOptions = [];
+
+    /**
+     * @inheritdoc
+     */
     public function init()
     {
         if ($this->username === null) {
-            $this->username = 'Alexander Pierce';
+            $this->username = '';
         }
-
         if ($this->userimg === null) {
-            $this->userimg = 'https://cdn0.iconfinder.com/data/icons/user-pictures/100/matureman1-2-128.png';
+            $this->userimg = '';
         }
-
         if ($this->usercreated === null) {
-            $this->usercreated = '2015-01-01 12:30:01';
+            $this->usercreated = '';
         }
-
-        if ($this->userbody === null) {
-            $this->userbody = true;
-        }
-
         if ($this->userbodyname1 === null) {
             $this->userbodyname1 = 'Followers';
         }
-
         if ($this->userbodylink1 === null) {
             $this->userbodylink1 = '#';
         }
-
         if ($this->userbodyname2 === null) {
             $this->userbodyname2 = 'Sales';
         }
-
         if ($this->userbodylink2 === null) {
             $this->userbodylink2 = '#';
         }
-
         if ($this->userbodyname3 === null) {
             $this->userbodyname3 = 'Friends';
         }
-
         if ($this->userbodylink3 === null) {
             $this->userbodylink3 = '#';
         }
-
-        if ($this->userfooter === null) {
-            $this->userfooter = true;
-        }
-
         if ($this->userfootername1 === null) {
             $this->userfootername1 = 'Profile';
         }
-
         if ($this->userfooterlink1 === null) {
             $this->userfooterlink1 = '#';
         }
-
         if ($this->userfootername2 === null) {
             $this->userfootername2 = 'Sign Out';
         }
-
         if ($this->userfooterlink2 === null) {
             $this->userfooterlink2 = '#';
         }
@@ -163,54 +155,67 @@ class NavbarUser extends Widget
         parent::init();
     }
 
-	/**
-	 * @return string
-	 */
-	public function run()
+    /**
+     * @return string
+     */
+    public function run()
     {
-        $html = '<li class="dropdown user user-menu">
-                    <a data-toggle="dropdown" class="dropdown-toggle" href="#">
-                          <img alt="'.Html::encode($this->username).'" class="user-image" src="'.Html::encode($this->userimg).'">
-                          <span class="hidden-xs">'.Html::encode($this->username).'</span>
-                    </a>
-                    <ul class="dropdown-menu">
-                        <li class="user-header">
-                            <img  alt="'.Html::encode($this->username).'" class="img-circle" src="'.Html::encode($this->userimg).'">
-                            <p>'.Html::encode($this->username).'
-                                <small>'.Html::encode($this->usercreated).'</small>
-                            </p>
-                        </li>';
+        $toggleContent = '';
+        if ($this->userimg !== null && $this->userimg !== '') {
+            $toggleContent .= Html::img($this->userimg, [
+                'class' => 'img-circle elevation-2',
+                'alt' => Html::encode($this->username),
+                'style' => 'width: 2rem; height: 2rem; object-fit: cover;',
+            ]);
+        }
+        $toggleContent .= Html::tag('span', Html::encode($this->username), ['class' => 'd-none d-md-inline ml-1']);
 
-        if($this->userbody):
-            $html .=   '<li class="user-body">
-                            <div class="row">
-                                <div class="col-xs-4 text-center">
-                                    <a href="'.Html::encode($this->userbodylink1).'">'.Html::encode($this->userbodyname1).'</a>
-                                </div>
-                                <div class="col-xs-4 text-center">
-                                    <a href="'.Html::encode($this->userbodylink2).'">'.Html::encode($this->userbodyname2).'</a>
-                                </div>
-                                <div class="col-xs-4 text-center">
-                                    <a href="'.Html::encode($this->userbodylink3).'">'.Html::encode($this->userbodyname3).'</a>
-                                </div>
-                            </div>
-                          </li>';
-        endif;
+        $linkOptions = array_merge([
+            'class' => 'nav-link',
+            'data-toggle' => 'dropdown',
+            'href' => '#',
+            'aria-haspopup' => 'true',
+            'aria-expanded' => 'false',
+        ], $this->linkOptions);
 
-        if($this->userfooter):
-            $html .=     '<li class="user-footer">
-                            <div class="pull-left">
-                                <a class="btn btn-default btn-flat" href="'.Html::encode($this->userfooterlink1).'">'.Html::encode($this->userfootername1).'</a>
-                            </div>
-                            <div class="pull-right">
-                                <a class="btn btn-default btn-flat" href="'.Html::encode($this->userfooterlink2).'">'.Html::encode($this->userfootername2).'</a>
-                            </div>
-                          </li>';
-        endif;
+        $toggle = Html::a($toggleContent, '#', $linkOptions);
 
-        $html .=       '</ul>
-                   </li>';
+        $menuItems = [];
 
-        return $html;
+        $headerContent = '';
+        if ($this->userimg !== null && $this->userimg !== '') {
+            $headerContent .= Html::img($this->userimg, [
+                'class' => 'img-circle elevation-2',
+                'alt' => Html::encode($this->username),
+                'style' => 'height: 90px; width: 90px; object-fit: cover;',
+            ]);
+        }
+        $headerContent .= Html::tag('p', Html::encode($this->username) . ($this->usercreated !== '' ? '<br><small>' . Html::encode($this->usercreated) . '</small>' : ''));
+        $menuItems[] = Html::tag('li', $headerContent, ['class' => 'user-header']);
+
+        if ($this->userbody) {
+            $bodyUrl1 = is_array($this->userbodylink1) ? Url::to($this->userbodylink1) : $this->userbodylink1;
+            $bodyUrl2 = is_array($this->userbodylink2) ? Url::to($this->userbodylink2) : $this->userbodylink2;
+            $bodyUrl3 = is_array($this->userbodylink3) ? Url::to($this->userbodylink3) : $this->userbodylink3;
+            $col1 = Html::tag('div', Html::a(Html::encode($this->userbodyname1), $bodyUrl1), ['class' => 'col-4 text-center']);
+            $col2 = Html::tag('div', Html::a(Html::encode($this->userbodyname2), $bodyUrl2), ['class' => 'col-4 text-center']);
+            $col3 = Html::tag('div', Html::a(Html::encode($this->userbodyname3), $bodyUrl3), ['class' => 'col-4 text-center']);
+            $bodyContent = Html::tag('div', $col1 . $col2 . $col3, ['class' => 'row']);
+            $menuItems[] = Html::tag('li', $bodyContent, ['class' => 'user-body']);
+        }
+
+        if ($this->userfooter) {
+            $footerUrl1 = is_array($this->userfooterlink1) ? Url::to($this->userfooterlink1) : $this->userfooterlink1;
+            $footerUrl2 = is_array($this->userfooterlink2) ? Url::to($this->userfooterlink2) : $this->userfooterlink2;
+            $footerContent = Html::tag('div', Html::a(Html::encode($this->userfootername1), $footerUrl1, ['class' => 'btn btn-default btn-flat']), ['class' => 'float-left']);
+            $footerContent .= Html::tag('div', Html::a(Html::encode($this->userfootername2), $footerUrl2, ['class' => 'btn btn-default btn-flat']), ['class' => 'float-right']);
+            $menuItems[] = Html::tag('li', $footerContent, ['class' => 'user-footer']);
+        }
+
+        $menu = Html::tag('ul', implode("\n", $menuItems), [
+            'class' => 'dropdown-menu dropdown-menu-right',
+        ]);
+
+        return Html::tag('li', $toggle . $menu, ['class' => 'nav-item dropdown user-menu']);
     }
 }
