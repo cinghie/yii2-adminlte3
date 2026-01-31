c9e20789a89079fa5335cde611205808bfdedc46
Giandomenico Olini <giando.olini@gogodigital.it>
2026-01-31 10:17:32 +0100

    Update NavbarLogo.php

--- widgets/NavbarLogo.php
+++ widgets/NavbarLogo.php
@@ -14,32 +14,80 @@ namespace cinghie\adminlte3\widgets;
 
 use Yii;
 use yii\bootstrap4\Widget;
+use yii\helpers\Html;
+use yii\helpers\Url;
 
 /**
- * Class NavbarLogo
+ * NavbarLogo (brand) widget for AdminLTE 3.
+ *
+ * Renders the sidebar brand link: logo image (optional) + app name.
+ * Use in sidebar: <aside class="main-sidebar"> ... <?= NavbarLogo::widget() ?> ... </aside>
+ *
+ * @see https://adminlte.io/docs/3.1/components/main-sidebar.html#brand-logo
  */
 class NavbarLogo extends Widget
 {
     /**
-     * @var string
+     * @var string|null URL of the brand image. Null = no image, only text.
      */
     public $logo_url;
 
-	/**
-	 * @return string
-	 */
-	public function run()
-    {
-        $html = '<a href="'.Yii::$app->homeUrl.'" class="brand-link text-center">';
+    /**
+     * @var string|null Brand text (default: Yii::$app->name)
+     */
+    public $brandText;
 
-        if($this->logo_url) {
-            $html .= '<img src="'.$this->logo_url.'" alt="'.Yii::$app->name.' Logo" class="brand-image img-circle elevation-3" style="opacity: .8">';
+    /**
+     * @var string|array Home URL (default: Yii::$app->homeUrl)
+     */
+    public $url;
+
+    /**
+     * @var array HTML options for the anchor (brand-link)
+     */
+    public $linkOptions = [];
+
+    /**
+     * @var array HTML options for the image
+     */
+    public $imageOptions = [];
+
+    /**
+     * @inheritdoc
+     */
+    public function init()
+    {
+        if ($this->brandText === null) {
+            $this->brandText = Yii::$app->name;
+        }
+        if ($this->url === null) {
+            $this->url = Yii::$app->homeUrl;
+        }
+        parent::init();
+    }
+
+    /**
+     * @return string
+     */
+    public function run()
+    {
+        $url = is_array($this->url) ? Url::to($this->url) : $this->url;
+
+        Html::addCssClass($this->linkOptions, 'brand-link');
+        Html::addCssClass($this->linkOptions, 'text-center');
+        $content = '';
+
+        if ($this->logo_url !== null && $this->logo_url !== '') {
+            $imgOptions = array_merge([
+                'alt' => Html::encode($this->brandText),
+                'class' => 'brand-image img-circle elevation-3',
+                'style' => 'opacity: .8',
+            ], $this->imageOptions);
+            $content .= Html::img($this->logo_url, $imgOptions);
         }
 
-        $html .= '<span class="brand-text font-weight-light">';
-        $html .= Yii::$app->name;
-        $html .= '</span></a>';
+        $content .= Html::tag('span', Html::encode($this->brandText), ['class' => 'brand-text font-weight-light']);
 
-        return $html;
+        return Html::a($content, $url, $this->linkOptions);
     }
 }
