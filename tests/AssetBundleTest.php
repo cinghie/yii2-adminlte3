1f59c859332bb10ef4537f46d2fb98b4244af4c0
Giandomenico Olini <giando.olini@gogodigital.it>
2026-08-19 10:54:10 +0200

    Test progress width asset publication

--- tests/AssetBundleTest.php
+++ tests/AssetBundleTest.php
@@ -6,6 +6,7 @@ namespace cinghie\adminlte3\tests;
 
 use cinghie\adminlte3\AdminLTEAsset;
 use cinghie\adminlte3\AdminLTEMinifyAsset;
+use cinghie\adminlte3\assets\AdminLTEThemeAsset;
 use PHPUnit\Framework\TestCase;
 use yii\bootstrap4\BootstrapAsset;
 use yii\web\JqueryAsset;
@@ -34,5 +35,18 @@ final class AssetBundleTest extends TestCase
         foreach ($asset->js as $script) {
             $this->assertStringNotContainsString('plugins/bootstrap/js/bootstrap', $script);
         }
+    }
+
+    public function testThemeAssetPublishesCspSafeProgressWidths(): void
+    {
+        $asset = new AdminLTEThemeAsset();
+
+        self::assertContains('css/progress-widths.css', $asset->css);
+
+        $css = file_get_contents(dirname(__DIR__) . '/assets/css/progress-widths.css');
+        self::assertIsString($css);
+        self::assertStringContainsString('.cinghie-progress-width-0 { width: 0%; }', $css);
+        self::assertStringContainsString('.cinghie-progress-width-37 { width: 37%; }', $css);
+        self::assertStringContainsString('.cinghie-progress-width-100 { width: 100%; }', $css);
     }
 }
