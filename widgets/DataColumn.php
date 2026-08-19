28069756ea317f7e07b3490a9d4ef592136e8324
Giandomenico Olini <giando.olini@gogodigital.it>
2026-08-19 11:40:24 +0200

    Normalize DataColumn source metadata

--- widgets/DataColumn.php
+++ widgets/DataColumn.php
@@ -1,15 +1,5 @@
 <?php
 
-/**
- * @copyright Copyright &copy; Gogodigital Srls
- * @company Gogodigital Srls - Wide ICT Solutions
- * @website http://www.gogodigital.it
- * @github https://github.com/cinghie/yii2-adminlte3
- * @license GNU GENERAL PUBLIC LICENSE VERSION 3
- * @package yii2-AdminLTE
- * @version 0.1.0
- */
-
 namespace cinghie\adminlte3\widgets;
 
 use kartik\grid\DataColumn as KartikDataColumn;
@@ -43,10 +33,11 @@ class DataColumn extends KartikDataColumn
 
         if ($this->attribute !== null && $this->enableSorting && ($sort = $provider->getSort()) !== false && $sort->hasAttribute($this->attribute)) {
             $direction = $sort->getAttributeOrder($this->attribute);
-            $sortClass = $direction !== null
-                ? 'sorting_' . ($direction === SORT_DESC ? 'desc' : 'asc')
-                : 'sorting';
-            Html::addCssClass($options, $sortClass);
+            if ($direction !== null) {
+                Html::addCssClass($options, $direction === SORT_DESC ? 'sorting_desc' : 'sorting_asc');
+            } else {
+                Html::addCssClass($options, 'sorting');
+            }
         }
 
         return Html::tag('th', $this->renderHeaderCellContent(), $options);
