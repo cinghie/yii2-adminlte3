98b745917331442005941fad9765c1dca77383de
Giandomenico Olini <giando.olini@gogodigital.it>
2026-08-19 12:37:52 +0200

    Support bundled and modular FullCalendar layouts

--- AdminLTECalendarAsset.php
+++ AdminLTECalendarAsset.php
@@ -2,6 +2,7 @@
 
 namespace cinghie\adminlte3;
 
+use Yii;
 use yii\bootstrap4\BootstrapAsset;
 use yii\web\AssetBundle;
 use yii\web\YiiAsset;
@@ -16,21 +17,43 @@ class AdminLTECalendarAsset extends AssetBundle
 
     public $css = [
         'plugins/fullcalendar/main.css',
-        'plugins/fullcalendar-daygrid/main.css',
-        'plugins/fullcalendar-timegrid/main.css',
-        'plugins/fullcalendar-bootstrap/main.css',
     ];
 
     public $js = [
         'plugins/fullcalendar/main.js',
-        'plugins/fullcalendar-interaction/main.js',
-        'plugins/fullcalendar-daygrid/main.js',
-        'plugins/fullcalendar-timegrid/main.js',
-        'plugins/fullcalendar-bootstrap/main.js',
     ];
 
     public $depends = [
         YiiAsset::class,
         BootstrapAsset::class,
     ];
+
+    /**
+     * {@inheritdoc}
+     */
+    public function init()
+    {
+        parent::init();
+
+        $root = rtrim(Yii::getAlias($this->sourcePath), '/\\') . DIRECTORY_SEPARATOR;
+        if (!is_file($root . 'plugins/fullcalendar-daygrid/main.css')) {
+            return;
+        }
+
+        // AdminLTE 3.0.x ships FullCalendar 4 plugin families separately.
+        // AdminLTE 3.1+ bundles them into plugins/fullcalendar/main.*.
+        $this->css = [
+            'plugins/fullcalendar/main.css',
+            'plugins/fullcalendar-daygrid/main.css',
+            'plugins/fullcalendar-timegrid/main.css',
+            'plugins/fullcalendar-bootstrap/main.css',
+        ];
+        $this->js = [
+            'plugins/fullcalendar/main.js',
+            'plugins/fullcalendar-interaction/main.js',
+            'plugins/fullcalendar-daygrid/main.js',
+            'plugins/fullcalendar-timegrid/main.js',
+            'plugins/fullcalendar-bootstrap/main.js',
+        ];
+    }
 }
