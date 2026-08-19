7007d17d1d889cfe4ba6820ce5d83a5c8e31f580
Giandomenico Olini <giando.olini@gogodigital.it>
2026-08-19 12:50:34 +0200

    Match AdminLTE 3 error page presentation

--- views/error/page.php
+++ views/error/page.php
@@ -1,26 +1,24 @@
 <?php
 
 use yii\helpers\Html;
-use yii\helpers\Url;
 
 $htmlOptions = $options;
 Html::addCssClass($htmlOptions, 'error-page');
-$homeHref = Url::to($homeUrl);
+$icon = Html::tag('i', '', [
+    'class' => 'fas fa-exclamation-triangle ' . $headlineClass,
+    'aria-hidden' => 'true',
+]);
+$titleHtml = $icon . ' ' . Html::encode($title);
+$messageHtml = nl2br(Html::encode($message));
+$homeLink = Html::a(Html::encode($homeLabel), $homeHref, ['aria-label' => $homeLabel]);
 ?>
 <section class="content">
     <?= Html::beginTag('div', $htmlOptions) ?>
         <?= Html::tag('h2', (string) $statusCode, ['class' => 'headline ' . $headlineClass]) ?>
         <div class="error-content">
-            <?= Html::tag('h3', Html::encode($title)) ?>
-            <?= Html::tag('p', nl2br(Html::encode($message))) ?>
-            <?= Html::a(
-                Html::tag('i', '', ['class' => 'fas fa-home mr-1', 'aria-hidden' => 'true']) . Html::encode($homeLabel),
-                $homeHref,
-                [
-                    'class' => 'btn btn-primary',
-                    'aria-label' => $homeLabel,
-                ]
-            ) ?>
+            <?= Html::tag('h3', $titleHtml) ?>
+            <?= Html::tag('p', $messageHtml) ?>
+            <?= Html::tag('p', 'Meanwhile, you may ' . $homeLink . '.') ?>
         </div>
     <?= Html::endTag('div') ?>
 </section>
