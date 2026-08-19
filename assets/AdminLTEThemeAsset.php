76408fdbb76d54d33e6488f887d9b041277c82f6
Giandomenico Olini <giando.olini@gogodigital.it>
2026-08-19 09:58:15 +0200

    Publish CSP-friendly widget script

--- assets/AdminLTEThemeAsset.php
+++ assets/AdminLTEThemeAsset.php
@@ -5,26 +5,45 @@ namespace cinghie\adminlte3\assets;
 use yii\web\AssetBundle;
 
 /**
- * Package theme / layout overrides for AdminLTE 3.
+ * Publishes package-owned AdminLTE theme overrides and widget behavior.
  */
 class AdminLTEThemeAsset extends AssetBundle
 {
+    /** @var string Source directory published by Yii's asset manager. */
     public $sourcePath = __DIR__;
+
+    /** @var bool Append source timestamps for browser cache invalidation. */
     public $appendTimestamp = true;
+
+    /** @var string[] Package CSS files. */
     public $css = [
         'css/adminlte-theme.css',
         'css/widgets.css',
     ];
+
+    /** @var string[] Package JavaScript files. */
+    public $js = [
+        'js/widgets.js',
+    ];
+
+    /** @var array Asset publication filter. */
     public $publishOptions = [
         'only' => [
             'css/*',
+            'js/*',
         ],
     ];
+
+    /** @var string[] No additional Yii AssetBundle dependencies. */
     public $depends = [];
 
+    /**
+     * {@inheritdoc}
+     */
     public function init()
     {
         parent::init();
+
         if (defined('YII_DEBUG') && YII_DEBUG) {
             $this->publishOptions['forceCopy'] = true;
         }
