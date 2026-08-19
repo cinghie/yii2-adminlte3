4aa7c0bd287e84fa5626fbc114dc4b0e42710a20
Giandomenico Olini <giando.olini@gogodigital.it>
2026-08-19 11:41:04 +0200

    Normalize ContentHeader source metadata

--- widgets/ContentHeader.php
+++ widgets/ContentHeader.php
@@ -1,15 +1,5 @@
 <?php
 
-/**
- * @copyright Copyright &copy; Gogodigital Srls
- * @company Gogodigital Srls - Wide ICT Solutions
- * @website http://www.gogodigital.it
- * @github https://github.com/cinghie/yii2-adminlte3
- * @license GNU GENERAL PUBLIC LICENSE VERSION 3
- * @package yii2-AdminLTE
- * @version 0.1.0
- */
-
 namespace cinghie\adminlte3\widgets;
 
 use cinghie\adminlte3\widgets\support\Translation;
@@ -101,6 +91,7 @@ class ContentHeader extends Widget
 
     /**
      * Renders the breadcrumbs block (AdminLTE 3 + Bootstrap 4 markup).
+     *
      * @return string
      */
     protected function renderBreadcrumbs()
