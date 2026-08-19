c58d07cdafb843b164ce61a8193b19c92c44ebf3
Giandomenico Olini <giando.olini@gogodigital.it>
2026-08-19 12:38:03 +0200

    Support bundled and modular minified FullCalendar layouts

--- AdminLTECalendarMinifyAsset.php
+++ AdminLTECalendarMinifyAsset.php
@@ -2,6 +2,7 @@
 
 namespace cinghie\adminlte3;
 
+use Yii;
 use yii\bootstrap4\BootstrapAsset;
 use yii\web\AssetBundle;
 use yii\web\YiiAsset;
@@ -16,21 +17,43 @@ class AdminLTECalendarMinifyAsset extends AssetBundle
 
     public $css = [
         'plugins/fullcalendar/main.min.css',
-        'plugins/fullcalendar-daygrid/main.min.css',
-        'plugins/fullcalendar-timegrid/main.min.css',
-        'plugins/fullcalendar-bootstrap/main.min.css',
     ];
 
     public $js = [
         'plugins/fullcalendar/main.min.js',
-        'plugins/fullcalendar-interaction/main.min.js',
-        'plugins/fullcalendar-daygrid/main.min.js',
-        'plugins/fullcalendar-timegrid/main.min.js',
-        'plugins/fullcalendar-bootstrap/main.min.js',
     ];
 
     public $depends = [
         YiiAsset::class,
         BootstrapAsset::class,
     ];
+
+    /**
+     * {@inheritdoc}
+     */
+    public function init()
+    {
+        parent::init();
+
+        $root = rtrim(Yii::getAlias($this->sourcePath), '/\\') . DIRECTORY_SEPARATOR;
+        if (!is_file($root . 'plugins/fullcalendar-daygrid/main.min.css')) {
+            return;
+        }
+
+        // AdminLTE 3.0.x ships FullCalendar 4 plugin families separately.
+        // AdminLTE 3.1+ bundles them into plugins/fullcalendar/main.*.
+        $this->css = [
+            'plugins/fullcalendar/main.min.css',
+            'plugins/fullcalendar-daygrid/main.min.css',
+            'plugins/fullcalendar-timegrid/main.min.css',
+            'plugins/fullcalendar-bootstrap/main.min.css',
+        ];
+        $this->js = [
+            'plugins/fullcalendar/main.min.js',
+            'plugins/fullcalendar-interaction/main.min.js',
+            'plugins/fullcalendar-daygrid/main.min.js',
+            'plugins/fullcalendar-timegrid/main.min.js',
+            'plugins/fullcalendar-bootstrap/main.min.js',
+        ];
+    }
 }
