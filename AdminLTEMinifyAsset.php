c0cbc78d03fcca2d32a1795175bfd81ff5d56241
Giandomenico Olini <giando.olini@gogodigital.it>
2026-08-19 11:25:15 +0200

    Convert minified AdminLTE asset into compatibility aggregate

--- AdminLTEMinifyAsset.php
+++ AdminLTEMinifyAsset.php
@@ -2,42 +2,24 @@
 
 namespace cinghie\adminlte3;
 
-use cinghie\adminlte3\assets\AdminLTEThemeAsset;
-use cinghie\fontawesome\FontAwesomeMinifyAsset;
-use yii\bootstrap4\BootstrapAsset;
 use yii\web\AssetBundle;
-use yii\web\YiiAsset;
 
 /**
- * AdminLTE 3 minified asset bundle.
+ * Backward-compatible aggregate minified AdminLTE 3 asset bundle.
+ *
+ * This convenience bundle preserves the historical plugin set. New pages that
+ * do not need jQuery UI, Tempus Dominus or iCheck should prefer
+ * {@see AdminLTECoreMinifyAsset} and register optional plugin bundles explicitly.
  */
 class AdminLTEMinifyAsset extends AssetBundle
 {
     public $sourcePath = '@vendor/almasaeed2010/adminlte/';
     public $appendTimestamp = true;
 
-    public $css = [
-        'plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css',
-        'plugins/icheck-bootstrap/icheck-bootstrap.min.css',
-        'dist/css/adminlte.min.css',
-    ];
-
-    public $js = [
-        'plugins/jquery-ui/jquery-ui.min.js',
-        'plugins/moment/moment.min.js',
-        'plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js',
-        'dist/js/adminlte.min.js',
-    ];
-
     public $depends = [
-        YiiAsset::class,
-        BootstrapAsset::class,
-        FontAwesomeMinifyAsset::class,
+        AdminLTEJqueryUiMinifyAsset::class,
+        AdminLTEDateTimeMinifyAsset::class,
+        AdminLTEIcheckMinifyAsset::class,
+        AdminLTECoreMinifyAsset::class,
     ];
-
-    public function registerAssetFiles($view)
-    {
-        parent::registerAssetFiles($view);
-        AdminLTEThemeAsset::register($view);
-    }
 }
