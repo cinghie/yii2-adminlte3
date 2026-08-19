cececf87106fc66a5b4ad814dd10a2e8e6fec50f
Giandomenico Olini <giando.olini@gogodigital.it>
2026-08-19 12:28:03 +0200

    Add ChartJS widget initializer asset

---
+++ assets/ChartJSWidgetAsset.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace cinghie\adminlte3\assets;
+
+use cinghie\adminlte3\AdminLTEChartJSMinifyAsset;
+use yii\web\AssetBundle;
+
+/**
+ * Publishes the package-owned ChartJS widget initializer.
+ */
+class ChartJSWidgetAsset extends AssetBundle
+{
+    public $sourcePath = __DIR__;
+    public $appendTimestamp = true;
+
+    public $js = [
+        'js/chartjs.js',
+    ];
+
+    public $publishOptions = [
+        'only' => [
+            'js/chartjs.js',
+        ],
+    ];
+
+    public $depends = [
+        AdminLTEChartJSMinifyAsset::class,
+    ];
+}
