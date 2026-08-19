3a726dabf320c4c55e25f3d2d30c4d2c2928d484
Giandomenico Olini <giando.olini@gogodigital.it>
2026-08-19 13:00:34 +0200

    Keep ChartJS presentation asset isolated

--- assets/ChartJSWidgetAsset.php
+++ assets/ChartJSWidgetAsset.php
@@ -6,12 +6,16 @@ use cinghie\adminlte3\AdminLTEChartJSMinifyAsset;
 use yii\web\AssetBundle;
 
 /**
- * Publishes the package-owned ChartJS widget initializer.
+ * Publishes the package-owned ChartJS widget presentation and initializer.
  */
 class ChartJSWidgetAsset extends AssetBundle
 {
     public $sourcePath = __DIR__;
     public $appendTimestamp = true;
+
+    public $css = [
+        'css/chartjs.css',
+    ];
 
     public $js = [
         'js/chartjs.js',
@@ -19,12 +23,12 @@ class ChartJSWidgetAsset extends AssetBundle
 
     public $publishOptions = [
         'only' => [
+            'css/chartjs.css',
             'js/chartjs.js',
         ],
     ];
 
     public $depends = [
         AdminLTEChartJSMinifyAsset::class,
-        AdminLTEThemeAsset::class,
     ];
 }
