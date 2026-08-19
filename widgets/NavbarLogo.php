baf5ca7e455599a317b87c8e423719bd3d12b3fc
Giandomenico Olini <giando.olini@gogodigital.it>
2026-08-19 10:01:49 +0200

    Make NavbarLogo URL and styling CSP-friendly

--- widgets/NavbarLogo.php
+++ widgets/NavbarLogo.php
@@ -1,59 +1,34 @@
 <?php
 
-/**
- * @copyright Copyright &copy; Gogodigital Srls
- * @company Gogodigital Srls - Wide ICT Solutions
- * @website http://www.gogodigital.it
- * @github https://github.com/cinghie/yii2-adminlte3
- * @license GNU GENERAL PUBLIC LICENSE VERSION 3
- * @package yii2-AdminLTE
- * @version 0.1.0
- */
-
 namespace cinghie\adminlte3\widgets;
 
+use cinghie\adminlte3\widgets\support\SafeHtml;
 use Yii;
 use yii\bootstrap4\Widget;
 use yii\helpers\Html;
-use yii\helpers\Url;
 
 /**
- * NavbarLogo (brand) widget for AdminLTE 3.
- *
- * Renders the sidebar brand link: logo image (optional) + app name.
- * Use in sidebar: <aside class="main-sidebar"> ... <?= NavbarLogo::widget() ?> ... </aside>
- *
- * @see https://adminlte.io/docs/3.1/components/main-sidebar.html#brand-logo
+ * Renders the AdminLTE 3 sidebar brand link.
  */
 class NavbarLogo extends Widget
 {
-    /**
-     * @var string|null URL of the brand image. Null = no image, only text.
-     */
+    /** @var string|null Optional brand image URL/path. */
     public $logo_url;
 
-    /**
-     * @var string|null Brand text (default: Yii::$app->name)
-     */
+    /** @var string|null Brand text; defaults to the Yii application name. */
     public $brandText;
 
-    /**
-     * @var string|array Home URL (default: Yii::$app->homeUrl)
-     */
+    /** @var string|array|null Home target; defaults to the application home URL. */
     public $url;
 
-    /**
-     * @var array HTML options for the anchor (brand-link)
-     */
+    /** @var array HTML options for the brand anchor. */
     public $linkOptions = [];
 
-    /**
-     * @var array HTML options for the image
-     */
+    /** @var array HTML options for the optional brand image. */
     public $imageOptions = [];
 
     /**
-     * @inheritdoc
+     * {@inheritdoc}
      */
     public function init()
     {
@@ -63,31 +38,32 @@ class NavbarLogo extends Widget
         if ($this->url === null) {
             $this->url = Yii::$app->homeUrl;
         }
+
         parent::init();
     }
 
     /**
-     * @return string
+     * {@inheritdoc}
      */
     public function run()
     {
-        $url = is_array($this->url) ? Url::to($this->url) : $this->url;
-
-        Html::addCssClass($this->linkOptions, 'brand-link');
-        Html::addCssClass($this->linkOptions, 'text-center');
+        Html::addCssClass($this->linkOptions, ['brand-link', 'text-center']);
         $content = '';
 
         if ($this->logo_url !== null && $this->logo_url !== '') {
             $imgOptions = array_merge([
                 'alt' => Html::encode($this->brandText),
-                'class' => 'brand-image img-circle elevation-3',
-                'style' => 'opacity: .8',
+                'class' => 'brand-image img-circle elevation-3 cinghie-brand-image',
             ], $this->imageOptions);
-            $content .= Html::img($this->logo_url, $imgOptions);
+            $content .= Html::img(SafeHtml::linkUrl($this->logo_url, ''), $imgOptions);
         }
 
-        $content .= Html::tag('span', Html::encode($this->brandText), ['class' => 'brand-text font-weight-light']);
+        $content .= Html::tag(
+            'span',
+            Html::encode($this->brandText),
+            ['class' => 'brand-text font-weight-light']
+        );
 
-        return Html::a($content, $url, $this->linkOptions);
+        return Html::a($content, SafeHtml::linkUrl($this->url, '#'), $this->linkOptions);
     }
 }
