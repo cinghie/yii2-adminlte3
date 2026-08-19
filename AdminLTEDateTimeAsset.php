60ee121407194894510e05503097c1c16a6eacf4
Giandomenico Olini <giando.olini@gogodigital.it>
2026-08-19 11:24:08 +0200

    Add optional Tempus Dominus asset bundle

---
+++ AdminLTEDateTimeAsset.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace cinghie\adminlte3;
+
+use yii\bootstrap4\BootstrapAsset;
+use yii\web\AssetBundle;
+use yii\web\YiiAsset;
+
+/**
+ * Optional AdminLTE date/time assets (Moment + Tempus Dominus Bootstrap 4).
+ */
+class AdminLTEDateTimeAsset extends AssetBundle
+{
+    public $sourcePath = '@vendor/almasaeed2010/adminlte/';
+    public $appendTimestamp = true;
+
+    public $css = [
+        'plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.css',
+    ];
+
+    public $js = [
+        'plugins/moment/moment.min.js',
+        'plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.js',
+    ];
+
+    public $depends = [
+        YiiAsset::class,
+        BootstrapAsset::class,
+    ];
+}
