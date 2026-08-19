2371cd56646f38d3d4d3ac21270638226c19ddc2
Giandomenico Olini <giando.olini@gogodigital.it>
2026-08-19 08:58:37 +0200

    Complete Yii test application configuration

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -5,16 +5,43 @@ declare(strict_types=1);
 require dirname(__DIR__) . '/vendor/autoload.php';
 require dirname(__DIR__) . '/vendor/yiisoft/yii2/Yii.php';
 
+$basePath = dirname(__DIR__);
+$runtimePath = $basePath . '/runtime';
+if (!is_dir($runtimePath)) {
+    mkdir($runtimePath, 0777, true);
+}
+
 if (Yii::$app === null) {
     new yii\web\Application([
         'id' => 'yii2-adminlte3-tests',
-        'basePath' => dirname(__DIR__),
-        'vendorPath' => dirname(__DIR__) . '/vendor',
+        'basePath' => $basePath,
+        'runtimePath' => $runtimePath,
+        'vendorPath' => $basePath . '/vendor',
+        'params' => [
+            'bsVersion' => '4.x',
+        ],
+        'modules' => [
+            'gridview' => [
+                'class' => kartik\grid\Module::class,
+            ],
+        ],
         'components' => [
             'request' => [
                 'cookieValidationKey' => 'test-key',
                 'scriptFile' => __FILE__,
                 'scriptUrl' => '/index.php',
+            ],
+            'i18n' => [
+                'translations' => [
+                    'traits' => [
+                        'class' => yii\i18n\PhpMessageSource::class,
+                        'basePath' => __DIR__ . '/messages',
+                    ],
+                    'crm' => [
+                        'class' => yii\i18n\PhpMessageSource::class,
+                        'basePath' => __DIR__ . '/messages',
+                    ],
+                ],
             ],
         ],
     ]);
