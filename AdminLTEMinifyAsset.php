174510c4cb5a1f8179a5667b8b32176a86a85ed1
Giandomenico Olini <giando.olini@gogodigital.it>
2026-08-19 08:44:42 +0200

    Deduplicate minified AdminLTE asset dependencies

--- AdminLTEMinifyAsset.php
+++ AdminLTEMinifyAsset.php
@@ -1,15 +1,5 @@
 <?php
 
-/**
- * @copyright Copyright &copy; Gogodigital Srls
- * @company Gogodigital Srls - Wide ICT Solutions
- * @website http://www.gogodigital.it
- * @github https://github.com/cinghie/yii2-adminlte3
- * @license BSD-3-Clause
- * @package yii2-adminlte3
- * @version 1.0.0
- */
-
 namespace cinghie\adminlte3;
 
 use cinghie\adminlte3\assets\AdminLTEThemeAsset;
@@ -17,63 +7,39 @@ use cinghie\fontawesome\FontAwesomeMinifyAsset;
 use cinghie\ionicons\IoniconsMinifyAsset;
 use yii\bootstrap4\BootstrapAsset;
 use yii\web\AssetBundle;
-use yii\web\JqueryAsset;
 use yii\web\YiiAsset;
 
 /**
  * AdminLTE 3 minified asset bundle.
- * appendTimestamp: cache busting when files change. In production, set forceCopy => false (default) for speed.
  */
 class AdminLTEMinifyAsset extends AssetBundle
 {
-	/**
-	 * @inheritdoc
-	 */
-	public $sourcePath = '@vendor/almasaeed2010/adminlte/';
+    public $sourcePath = '@vendor/almasaeed2010/adminlte/';
+    public $appendTimestamp = true;
 
-	/**
-	 * @inheritdoc
-	 */
-	public $appendTimestamp = true;
+    public $css = [
+        'plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css',
+        'plugins/icheck-bootstrap/icheck-bootstrap.min.css',
+        'dist/css/adminlte.min.css',
+    ];
 
-	/**
-	 * @inheritdoc
-	 */
-	public $css = [
-		'plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css',
-		'plugins/icheck-bootstrap/icheck-bootstrap.min.css',
-		'dist/css/adminlte.min.css',
-	];
+    public $js = [
+        'plugins/jquery-ui/jquery-ui.min.js',
+        'plugins/moment/moment.min.js',
+        'plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js',
+        'dist/js/adminlte.min.js',
+    ];
 
-	/**
-	 * @inheritdoc
-	 */
-	public $js = [
-		'plugins/jquery-ui/jquery-ui.min.js',
-		'plugins/bootstrap/js/bootstrap.bundle.min.js',
-		'plugins/moment/moment.min.js',
-		'plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js',
-		'dist/js/adminlte.min.js',
-	];
+    public $depends = [
+        YiiAsset::class,
+        BootstrapAsset::class,
+        FontAwesomeMinifyAsset::class,
+        IoniconsMinifyAsset::class,
+    ];
 
-	/**
-	 * @inheritdoc
-	 */
-	public $depends = [
-		YiiAsset::class,
-		JqueryAsset::class,
-		BootstrapAsset::class,
-		FontAwesomeMinifyAsset::class,
-		IoniconsMinifyAsset::class,
-	];
-
-	/**
-	 * @inheritdoc
-	 */
-	public function registerAssetFiles($view)
-	{
-		parent::registerAssetFiles($view);
-		// Theme overrides after core AdminLTE CSS (exportable with this package).
-		AdminLTEThemeAsset::register($view);
-	}
+    public function registerAssetFiles($view)
+    {
+        parent::registerAssetFiles($view);
+        AdminLTEThemeAsset::register($view);
+    }
 }
