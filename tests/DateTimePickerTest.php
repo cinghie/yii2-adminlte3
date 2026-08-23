1569011612d1081bacf4900474c71e658e0401fc
Giandomenico Olini <giando.olini@gogodigital.it>
2026-08-23 18:15:03 +0200

    [skip ci] test AdminLTE3 datetime picker contract

---
+++ tests/DateTimePickerTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace cinghie\adminlte3\tests;
+
+use PHPUnit\Framework\TestCase;
+
+class DateTimePickerTest extends TestCase
+{
+    public function testWidgetUsesBundledTempusDominusWithPrependedIcon(): void
+    {
+        $src = file_get_contents(dirname(__DIR__) . '/widgets/DateTimePicker.php');
+
+        $this->assertStringContainsString('AdminLTEDateTimeAsset::register', $src);
+        $this->assertStringContainsString("'class' => 'input-group-prepend'", $src);
+        $this->assertStringContainsString("'data-toggle' => 'datetimepicker'", $src);
+        $this->assertStringContainsString("'data-target-input' => 'nearest'", $src);
+        $this->assertStringContainsString("datetimepicker({$config})", $src);
+    }
+}
